afd37bf181b2e302a5aa720bd863cd348d75894e
ayadfadhil <ayad.jumaah@gmail.com>
2024-01-17 19:50:44 +0100

    added the fisrt link files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import About from "./About";
+import Home from "./Home";
+import { Link, Route, Routes } from "react-router-dom";
+import Vans from "./Vans";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header>
+        <Link className="site-logo" to="/">
+          #VanLife
+        </Link>
+        <nav>
+          <Link to="/about">About</Link>
+
+          <Link to="/vans">Vans</Link>
+        </nav>
       </header>
-    </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/vans" element={<Vans />} />
+      </Routes>
+    </>
   );
 }
 
---
+++ src/Vans.js
@@ -0,0 +1,13 @@
+import React from "react";
+import bgImg from "./images/about-hero.png";
+
+function Vans() {
+  return (
+    <div className="div-van">
+      <img src={bgImg} alt="vanimg" className="about-hero-image" />
+      <div>Vans</div>
+    </div>
+  );
+}
+
+export default Vans;
